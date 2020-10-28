82b1429d591d523f8677c009c2e1b65b3e0218c8
Thomas Brandl <thomas.brandl@dynatrace.com>
2020-10-28 16:19:09 +0000

    Allow to send metrics using API as well

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dynatrace-nagios
 
-This simple shell script allows to get metrics from Nagios plugins and directly send them to Dynatrace using dynatrace_ingest.
+This simple shell script allows to get metrics from Nagios plugins and directly send them to Dynatrace using dynatrace_ingest/Metric API v2.
 # Usage
 ```bash
 ./dynatrace_nagios.sh [OPTIONS] "[nagios_command_including_parameters]"
@@ -11,6 +11,8 @@ Short option | Long Option | Parameter | Description
 ------------ | ----------- | --------- | -----------
 -d | --dimension | DIMENSION  | The metric name from Nagios will be treated as dimension of name DIMENSION instead. This is useful e.g. when the metric name denominates a disk etc.
 -a | --additional-dimension | ADDITIONAL_DIMENSION | Adds additional static dimensions in Dynatrace - e.g. if you want to assign a metric to a certain Application.
+-e | --endpoint | ENDPOINT | Allows to send metrics through ActiveGate/Dynatrace server instead of dynatrace_ingest for hosts without OneAgent
+-k | --key | API_TOKEN | Sets the API token to use when sending metrics through ActiveGate/Dynatrace server
 -h | --dynatrace-home | DT_HOME | This is required to set if the OneAgent is not installed in /opt/dynatrace/oneagent
 -n | --prefix | PREFIX | The prefix to use for this metric before the command. The default is "nagios."
 -p | --port | PORT | The port to use for sending the metrics (default 14499)
@@ -26,4 +28,5 @@ Short option | Long Option | Parameter | Description
 ./dynatrace_nagios.sh -d disk "/usr/lib/nagios/plugins/check_disk -w 95 -c 98"
 ./dynatrace_nagios.sh "/usr/lib/nagios/plugins/check_tcp -p 22" -a protocol=ssh -v
 ./dynatrace_nagios.sh "/usr/lib/nagios/plugins/check_host www.amazon.com" -a dt.entity.application=APPLICATION-073FCAFAFDEAAC57 --test
+./dynatrace_nagios.sh /usr/lib/nagios/plugins/check_swap -w 90 -e https://xxxxxxxx.live.dynatrace.com/api/v2/metrics/ingest -k xxxxxxxxxxxxxxx
 ```
--- dynatrace_nagios.sh
+++ dynatrace_nagios.sh
@@ -5,6 +5,8 @@ usage () {
 	echo "OPTIONS can be one of:"
 	echo -e "\t-d|--dimension DIMENSION The metric name from Nagios will be treated as dimension of name DIMENSION instead. This is useful e.g. when the metric name denominates a disk etc."
         echo -e "\t-a|--additional-dimension ADDITIONAL_DIMENSION Adds additional static dimensions in Dynatrace - e.g. if you want to assign a metric to a certain Application."
+	echo -e "\t-e|--endpoint ENDPOINT Allows to send metrics through ActiveGate/Dynatrace server instead of dynatrace_ingest for hosts without OneAgent"
+	echo -e "\t-k|--key API_TOKEN Sets the API token to use when sending metrics through ActiveGate/Dynatrace server"
         echo -e "\t-h|--dynatrace-home DT_HOME This is required to set if the OneAgent is not installed in /opt/dynatrace/oneagent"
         echo -e "\t-n|--prefix PREFIX The prefix to use for this metric before the command. The default is \"nagios.\""
 	echo -e "\t-p|--port PORT The port to use for sending the metrics (default 14499)"
@@ -17,6 +19,7 @@ usage () {
 	echo -e "\t$0 -d disk \"/usr/lib/nagios/plugins/check_disk -w 95 -c 98\""
 	echo -e "\t$0 \"/usr/lib/nagios/plugins/check_tcp -p 22\" -a protocol=ssh -v"
 	echo -e "\t$0 \"/usr/lib/nagios/plugins/check_host www.amazon.com\" -a dt.entity.application=APPLICATION-073FCAFAFDEAAC57 --test"
+	echo -e "\t$0 "/usr/lib/nagios/plugins/check_swap -w 90" -e https://xxxxxxxx.live.dynatrace.com/api/v2/metrics/ingest -k xxxxxxxxxxxxxxx"
 	exit 1	
 }
 
@@ -24,6 +27,8 @@ while [[ "$#" -gt 0 ]]; do
 	case $1 in
 		-d|--dimension) DIMENSION="$2"; shift ;;
 		-a|--additional-dimension) ADDITIONAL_DIMENSION="$2"; shift;;
+		-e|--endpoint) DT_ENDPOINT="$2"; shift;;
+		-k|--key) API_TOKEN="$2"; shift;;
 		-h|--dynatrace-home) DT_HOME="$2"; shift;;
 		-n|--prefix) DT_NAGIOS_PREFIX="$2"; shift;;
 		-p|--port) DT_PORT="-p $2 "; shift;;
@@ -45,8 +50,12 @@ fi
 
 DYNATRACE_INGEST="$DT_HOME"/agent/tools/dynatrace_ingest
 
-if [ ! -f "$DYNATRACE_INGEST" ]; then
-	echo "dynatrace_ingest not found. Check whether the Dynatrace OneAgent is installed, and if it is installed in a non-standard directory, set DT_HOME first"
+if [ -n "$DT_ENDPOINT" -a -z "$API_TOKEN" ]; then
+                echo "Sending metrics via the API requires a valid API-Token (-k|--key)."
+                exit 3
+elif [ ! -f "$DYNATRACE_INGEST" ]; then
+	echo "dynatrace_ingest not found. Check whether the Dynatrace OneAgent is installed, and if it is installed in a non-standard directory, use -h [DT_HOME]."
+	echo "Alternatively, you can send metrics using the API-this requires to set the endpoint (-e|--endpoint) and API Token (-k|--key)."
 	exit 2
 fi
 
@@ -77,5 +86,12 @@ DYNATRACE_METRICS=`echo $NAGIOS_PERFDATA|awk 'BEGIN { RS = " " } {split($0,metri
 if [ "$TEST_ONLY" == "1" ]; then
 	echo $DYNATRACE_METRICS
 else
-	$DYNATRACE_INGEST $VERBOSE$DT_PORT"$DYNATRACE_METRICS"
+	if [ -z "$DT_ENDPOINT" ]; then
+		$DYNATRACE_INGEST $VERBOSE$DT_PORT"$DYNATRACE_METRICS"
+	else
+		CURL_OUTPUT=$(curl -H "Content-Type: text/plain; charset=utf-8" -H "Authorization: Api-Token $API_TOKEN" -d "$DYNATRACE_METRICS" "$DT_ENDPOINT" 2>/dev/null)
+		if [ -n "$VERBOSE" ]; then
+			echo $CURL_OUTPUT
+		fi
+	fi
 fi
