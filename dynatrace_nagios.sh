4a245175b9d5c9265cdcd9006a8b5a709e618ae7
Thomas Brandl <thomas.brandl@dynatrace.com>
2020-10-28 13:14:58 +0000

    added more examples

--- dynatrace_nagios.sh
+++ dynatrace_nagios.sh
@@ -15,7 +15,7 @@ usage () {
         echo "Examples:"
        	echo -e "\t$0 \"/usr/lib/nagios/plugins/check_load -r\""
 	echo -e "\t$0 -d disk \"/usr/lib/nagios/plugins/check_disk -w 95 -c 98\""
-	echo -e "\t$0"
+	echo -e "\t$0 \"/usr/lib/nagios/plugins/check_tcp -p 22\" -a protocol=ssh -v"
 	exit 1	
 }
 
